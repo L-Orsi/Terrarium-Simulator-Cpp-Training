79cf488d85e930891ca95305bb56a4f7482888c1
Lucas Orsi <lorsi@google.com>
2020-09-20 13:31:22 -0300

    Added enging behavior and missing classes code. First fully functional and working version

--- src/simulation_map.cpp
+++ src/simulation_map.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
-#include <array>
+#include <vector>
 #include <memory>
 
 #include <simulator/simulation_map.hpp>
 #include <simulator/map_cell.hpp>
+#include <simulator/bug.hpp>
 
 namespace simulation {
 
-SimulationMap::SimulationMap() {}
+SimulationMap::SimulationMap(uint8_t rows, uint8_t cols): rows_(rows), cols_(cols) {
+  for(auto k=0; k<rows*cols; k++) {
+    cells_.push_back(std::make_shared<MapCell>(nullptr));
+  }
+  for(auto i=0; i<rows; i++) {
+    for(auto j=0; j<cols; j++) {
+      cells_[i*cols+j]->set_adjacents(
+        {
+          (j+1 >= cols) ? nullptr : cells_[i*cols+(j+1)],
+          (i-1 < 0) ? nullptr : cells_[(i-1)*cols+j],
+          (j-1 < 0) ? nullptr : cells_[i*cols+(j-1)],
+          (i+1 >= rows) ? nullptr : cells_[(i+1)*cols+(j)],
+        }
+      );
+    }
+  }
+}
 
-std::array<const std::shared_ptr<MapCell>, CELLS_TOTAL>& SimulationMap::get_cells() {
+std::vector<std::shared_ptr<MapCell>>& SimulationMap::get_cells() {
   return cells_;
+}
+
+void SimulationMap::print_map() {
+    for(auto i=0; i<rows_; i++) {
+      std::cout << std::endl;
+      for(auto j=0; j<cols_; j++) {
+        auto bg = cells_[i*cols_+j]->get_bug();
+        if(!bg){
+          std::cout << "[ ]";
+        } else {
+          std::cout << bg->get_name();
+        }
+          
+        std::cout << "\t";
+      }
+    }
+     std::cout << std::endl;
 }
 
 } // namespace: simulation
